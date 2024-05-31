730d6571f3e96fde5d2f418ca0647eef3e6c1ad3
Bhaumik Tiwari <97089417+BhaumikTiwari@users.noreply.github.com>
2024-05-31 15:41:59 +0530

    Add files via upload

---
+++ capture_image_from_camera.py
@@ -0,0 +1,19 @@
+import cv2
+from cv2 import *
+cam_port = 0
+cam =cv2.VideoCapture(0)
+# reading the input using the camera
+
+inp = input('Enter person name')
+# If image will detected without any error,
+# show result
+while(1):
+        result,image = cam.read()
+        cv2.imshow(inp, image)
+        if cv2.waitKey(0):
+         cv2.imwrite(inp+".png", image)
+         print("image taken")
+
+# If captured image is corrupted, moving to else part
+else:
+	print("No image detected. Please! try again")
